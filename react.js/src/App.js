d589a095cb2f7b60293df3a4a90971166bdb4036
may-hj <hyeonjeong9943@gmail.com>
2021-04-16 11:22:34 +0900

    How to Route

--- react.js/src/App.js
+++ react.js/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import ArticleDetail from './board/component/ArticleDetail'
+import ArticleList from './board/component/ArticleList'
+import ArticleUpdate from './board/component/ArticleUpdate'
+import ArticleWrite from './board/component/ArticleWrite'
+import Login from './user/component/Login'
+import Signup from './user/component/Signup'
+import UserDetail from './user/component/UserDetail'
 
 function App() {
   return (
     <div className="App">
-      <h1>Start React 200!</h1>
-      <p>HTML 적용하기</p>
+      <Route exact path='/board/article-detail' component={ArticleDetail}></Route>
+      <Route exact path='/board/article-list' component={ArticleList}></Route>
+      <Route exact path='/board/article-update' component={ArticleUpdate}></Route>
+      <Route exact path='/board/article-write' component={ArticleWrite}></Route>
+      <Route exact path='/user/login' component={Login}></Route>
+      <Route exact path='/user/signup' component={Signup}></Route>
+      <Route exact path='/user/user-detail' component={UserDetail}></Route>
     </div>
   );
 }
